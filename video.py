806408929860ae19c9b969cc0694e2ea25977051
maayan <depisky@gmail.com>
2024-05-03 10:06:53 -0600

    first come

---
+++ video.py
@@ -0,0 +1,27 @@
+ import cv2
+ import threading
+
+
+# def stop_pls():
+#     print("STOP")
+
+
+# def unpause_video():
+#     global paused
+#     paused = False
+
+
+# paused = False
+
+def play_vid(paused):
+    cap = cv2.VideoCapture('C:\LAB\car tech\carvid.mp4')
+    while True:
+        ret, frame =cap.read()
+        if ret:
+            if paused==False:
+                cv2.imshow('Video', frame)
+                if cv2.waitKey(25) & 0xFF == ord('q'):
+                    cv2.destroyAllWindows()
+                    cap.release()
+                    break
+            
